80376e24e27f5ceb28b47071167b83434416106f
Aluno do Senac <vkelven@hotmail.com>
2018-04-03 18:46:29 -0300

    Alterado a declaração da função

--- senac-cn-ado01/lib.h
+++ senac-cn-ado01/lib.h
@@ -2,3 +2,5 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+
+void newBase(int , char **);
--- senac-cn-ado01/n2m.c
+++ senac-cn-ado01/n2m.c
@@ -1,4 +1,4 @@
-void newBase(int , char **);
+#include "lib.h"
 
 int main(int argc, char *argv[]){
 
